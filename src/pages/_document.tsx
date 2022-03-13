bac56afaa50c371ffac71fe6b4ed39dc92e17ed9
Ziphil <yfe23457@nifty.com>
2022-03-13 21:15:27 +0900

    Update OGP and others

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,23 @@ import NextDocument, { Head, Html, Main, NextScript } from "next/document"
 import React from "react"
 
 
+const TITLE = "Learning Animals"
+const DESCRIPTION = "A quiz-themed web3 application, which provides a place where you can learn and earn by playing quizzes."
+
 class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html lang="ja">
         <Head>
+          <title>{TITLE}</title>
+          <meta name="description" content={DESCRIPTION} />
+          <meta property="og:url" content="http://learninganimals.io" />
+          <meta property="og:title" content={TITLE} />
+          <meta property="og:site_name" content={TITLE} />
+          <meta property="og:description" content={DESCRIPTION} />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="/ogp.png" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;700&display=swap" rel="stylesheet" />
