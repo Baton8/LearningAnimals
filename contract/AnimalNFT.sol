0267876fe860544a7cd65582bd3ef988e90a3669
aminaura <aura.otsuka@gmail.com>
2022-03-13 23:16:39 +0900

    .

--- contract/AnimalNFT.sol
+++ contract/AnimalNFT.sol
@@ -10,27 +10,33 @@ import "hardhat/console.sol";
 contract AnimalNFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
-    mapping(uint256 => uint256) private _truck;
+
+    mapping(uint => uint) private trackIdOf; // related track ID of token ID
 
     constructor () ERC721 ("LearningAnimals", "LA NFT") {}
     
-   function mint(uint256 truckId, address _sender) public returns (uint256) {
+   function mint(address _address, uint _trackId) public returns (uint256) {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _truck[tokenId] = truckId;
-
-        _safeMint(_sender, tokenId);
-
+        trackIdOf[tokenId] = _trackId;
+        _safeMint(_address, tokenId);
         return tokenId;
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
-        return "https://gateway.pinata.cloud/ipfs/QmT1nejTfKRpngZd5wnPd5T2cgx8rGpKUHZXLArdPYTfWQ/";
+        return "https://gateway.pinata.cloud/ipfs/QmYKJ4TieKdJ9CPZF4RkrzJ9dzLKknwPK5AGsCgJbW74hx/";
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         string memory base = _baseURI();
-        return string(abi.encodePacked(base, Strings.toString(tokenId)));
+        uint remainder = tokenId % 8;
+        string memory name;
+        if (remainder <= 2) {
+            name = Strings.toString(remainder);
+        } else {
+            name = "quiz";
+        }
+        return string(abi.encodePacked(base, trackIdOf[tokenId], "_", name, ".json"));
     }
 
     function transfer(address from, address to, uint256 tokenId) public {
